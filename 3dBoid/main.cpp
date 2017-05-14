7f1cb50a6b1121f4162c8ccee170cc8f6b805066
Ryota <ryota6621@gmail.com>
2017-05-14 11:33:38 +0900

    Initial Commit

---
+++ 3dBoid/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  3dBoid
+//
+//  Created by Ryota Ishidu on 2017/05/14.
+//  Copyright © 2017年 Ryota Ishidu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
